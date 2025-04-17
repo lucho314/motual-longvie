b41ec4c181344d9c430e09471659b3e58caaf3b9
Luciano Zapata <lzapata@ater.gob.ar>
2025-04-17 20:30:42 -0300

    Agregar clase PruebaMail y vista de correo de prueba, incluyendo definición de contenido y estructura HTML.

---
+++ resources/views/emails/prueba.blade.php
@@ -0,0 +1,11 @@
+<!-- resources/views/emails/prueba.blade.php -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Correo de prueba</title>
+</head>
+<body>
+    <h1>¡Hola Luciano!</h1>
+    <p>Este es un correo enviado desde Laravel con amor ❤️.</p>
+</body>
+</html>
